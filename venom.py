b76aa20b8da40f438e0602724c727edaf7c50b12
Animefriend24 <animefriend.123123@gmail.com>
2024-07-31 10:36:04 +0530

    Update venom.py

--- venom.py
+++ venom.py
@@ -4,8 +4,8 @@ import logging
 from aiogram import Bot
 import asyncio
 
-API_TOKEN = 'TOKEN'
-ADMIN_ID = 'YOUR ID'
+API_TOKEN = '7474551048:AAHFKRmljJokDByqX762G-089GZV4EC3Syw'
+ADMIN_ID = '1694123385'
 MAX_RESTARTS = 5
 RESTART_PERIOD = 60  # Seconds
 
